f5e02795bb3c347f49637e27014516291c381156
Andrei <85843872+Andrei1808@users.noreply.github.com>
2021-11-01 21:26:43 +0200

    31.10.21

---
+++ fcc3.js
@@ -0,0 +1,103 @@
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+let catName = "Oliver";
+let catSound = "Meow!";
+
+// Only change code below this line
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+// Only change code above this line
+
+fact = "is awesome!";
+console.log(FCC, fact);
+
+const sum = 10 + 10;
+
+const difference = 45 - 33;
+
+const product = 8 * 10;
+
+const quotient = 66 / 33;
+
+let myVar = 87;
+
+// Only change code below this line
+myVar ++;
+
+let myVar = 11;
+
+// Only change code below this line
+myVar --;
+
+const myDecimal = 5.7;
+
+const product = 2.0 * 2.5;
+
+const quotient = 4.4 / 2.0; // Change this line
+
+const remainder = 11 % 3;
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -=1;
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+
+
